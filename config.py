92803cfbfae4828865d39e2eab139b5a736dd9ec
Yakup Kaplan <yakup.kaplan006@gmail.com>
2022-06-04 14:59:30 +0200

    Project structure updated. Source file added. Pipfile for package management added

---
+++ config.py
@@ -0,0 +1,10 @@
+"""
+Config Script
+"""
+
+# Define paths
+ROOT = "C:/Users/yakup/PycharmProjects/datatalksclub-mlops-zoomcamp/"
+PATH_DATA_GT_JAN = ROOT + "data/green_tripdata_2021-01.parquet"
+PATH_DATA_GT_FEB = ROOT + "data/green_tripdata_2021-02.parquet"
+PATH_DATA_FHV_JAN = ROOT + "data/fhv_tripdata_2021-01.parquet"
+PATH_DATA_FHV_FEB = ROOT + "data/fhv_tripdata_2021-02.parquet"
